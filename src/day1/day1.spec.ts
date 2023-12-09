e39125f92c1589ac2c35938e1d0522edadbfe92c
DenshiJisho <123274595+DenshiJisho@users.noreply.github.com>
2023-12-09 18:24:50 -0500

    Part 2 almost works

--- src/day1/day1.spec.ts
+++ src/day1/day1.spec.ts
@@ -1,12 +1,26 @@
 import day1 from './index';
 
-const example_input = `1abc2
+const example_input1 = `1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
-treb7uchet`
+treb7uchet`;
+
+const example_input2 = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
 
 describe('On Day 1', () =>{
     it(`part1 example`, ()=>{
-        expect(day1.solveForPartOne(example_input)).toBe("142");
+        expect(day1.solveForPartOne(example_input1)).toBe("142");
+    })
+});
+
+describe('On Day 1', () =>{
+    it(`part2 example`, ()=>{
+        expect(day1.solveForPartTwo(example_input2)).toBe("281");
     })
 });
--- src/day1/index.ts
+++ src/day1/index.ts
@@ -2,27 +2,52 @@ import { Day } from "../day";
 
 class Day1 extends Day {
 
+    private NUMBERS: {[name:string]: number} = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9
+    };
+
     constructor(){
         super(1);
     }
 
-    solveForPartOne(input: string): string {
-        let total: number = 0;
-        const regex: RegExp = /\d/g;
-        let matches: RegExpMatchArray | null;
-        for (const line of input.split("\n")) {
-            matches = line.match(regex);
-            if (!matches) {
-                continue;
-            }
-            total += parseInt(matches[0]) * 10;
-            total += parseInt(matches[matches.length - 1]);
+    private matchToInt(match: string): number {
+        if (match in this.NUMBERS) {
+            return this.NUMBERS[match];
+        } else {
+            return parseInt(match);
         }
-        return String(total);
+    }
+
+    private calibrate(lines: string[], regex: RegExp): number {
+        let matches: RegExpMatchArray | null;
+        return lines.reduce(
+            (acc, val) => {
+                matches = val.match(regex);
+                console.log(matches);
+                if (matches) {
+                    acc += this.matchToInt(matches[0]) * 10;
+                    acc += this.matchToInt(matches[matches.length - 1]);
+                }
+                return acc;
+            }, 0 // initial value
+        );
+    }
+
+    solveForPartOne(input: string): string {
+        return String(this.calibrate(input.split("\n"), /\d/g));
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+        let regex = RegExp(Object.keys(this.NUMBERS).join('|') + "|\\d", 'g'); // names, digits, global match
+        return String(this.calibrate(input.split("\n"), regex));
     }
 }
 
