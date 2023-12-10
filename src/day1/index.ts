88a5c72e19e6be58d232539f2c79d80fd96993ba
DenshiJisho <123274595+DenshiJisho@users.noreply.github.com>
2023-12-09 19:18:58 -0500

    linted day 1

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,52 +1,52 @@
 import { Day } from '../day'
 
 class Day1 extends Day {
-    private NUMBERS: {[name:string]: number} = {
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9
-    };
+  private NUMBERS: {[name:string]: number} = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9
+  };
 
-    constructor(){
-        super(1);
+  constructor () {
+    super(1);
+  }
+
+  private matchToInt (match: string): number {
+    if (match in this.NUMBERS) {
+      return this.NUMBERS[match];
+    } else {
+      return parseInt(match);
     }
+  }
 
-    private matchToInt(match: string): number {
-        if (match in this.NUMBERS) {
-            return this.NUMBERS[match];
-        } else {
-            return parseInt(match);
+  private calibrate (lines: string[], regex: RegExp): number {
+    let matches: string[];
+    return lines.reduce(
+      (acc, val) => {
+        matches = Array.from(val.matchAll(regex), x => x[1]); // extract capture groups
+        if (matches) {
+          acc += this.matchToInt(matches[0]) * 10;
+          acc += this.matchToInt(matches[matches.length - 1]);
         }
-    }
+        return acc;
+      }, 0 // initial value
+    );
+  }
 
-    private calibrate(lines: string[], regex: RegExp): number {
-        let matches: string[];
-        return lines.reduce(
-            (acc, val) => {
-                matches = Array.from(val.matchAll(regex), x => x[1]); // extract capture groups
-                if (matches) {
-                    acc += this.matchToInt(matches[0]) * 10;
-                    acc += this.matchToInt(matches[matches.length - 1]);
-                }
-                return acc;
-            }, 0 // initial value
-        );
-    }
+  solveForPartOne (input: string): string {
+    return String(this.calibrate(input.split('\n'), /(\d)/g));
+  }
 
-    solveForPartOne (input: string): string {
-        return String(this.calibrate(input.split('\n'), /(\d)/g));
-    }
-
-    solveForPartTwo (input: string): string {
-        const regex = RegExp(`(?=(${Object.keys(this.NUMBERS).join('|')}|\\d))`, 'g'); // names, digits, global match, positive lookahead for overlaps
-        return String(this.calibrate(input.split('\n'), regex));
-    }
+  solveForPartTwo (input: string): string {
+    const regex = RegExp(`(?=(${Object.keys(this.NUMBERS).join('|')}|\\d))`, 'g'); // names, digits, global match, positive lookahead for overlaps
+    return String(this.calibrate(input.split('\n'), regex));
+  }
 }
 
 export default new Day1();
