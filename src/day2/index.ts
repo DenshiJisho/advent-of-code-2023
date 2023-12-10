035a5ec05bcd9b8a3815de0b9f13cd0247f71b27
DenshiJisho <123274595+DenshiJisho@users.noreply.github.com>
2023-12-10 01:12:41 -0500

    Init day 2

---
+++ src/day2/index.ts
@@ -0,0 +1,17 @@
+import { Day } from '../day';
+
+class Day2 extends Day {
+  constructor () {
+    super(2);
+  }
+
+  solveForPartOne (input: string): string {
+    return input;
+  }
+
+  solveForPartTwo (input: string): string {
+    return input;
+  }
+}
+
+export default new Day2();
