705f191c37bb8f4d32599696c5e3788c4c728f1c
DenshiJisho <123274595+DenshiJisho@users.noreply.github.com>
2023-12-09 19:23:11 -0500

    lint day0

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Advent of Code 2023
 
 My personal attempts and solutions
+
+Using [LBognanni's AoC template framework](https://github.com/LBognanni/adventofcode-typescript-starter)
--- src/day0/index.ts
+++ src/day0/index.ts
@@ -1,18 +1,17 @@
-import { Day } from "../day";
+import { Day } from '../day';
 
 class Day0 extends Day {
+  constructor () {
+    super(0);
+  }
 
-    constructor(){
-        super(0);
-    }
+  solveForPartOne (input: string): string {
+    return input;
+  }
 
-    solveForPartOne(input: string): string {
-        return input;
-    }
-
-    solveForPartTwo(input: string): string {
-        return input;
-    }
+  solveForPartTwo (input: string): string {
+    return input;
+  }
 }
 
-export default new Day0;
+export default new Day0();
