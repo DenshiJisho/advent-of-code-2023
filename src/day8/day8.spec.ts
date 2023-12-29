74852518922bf3cadab62bdcff40e3fcf5e57979
DenshiJisho <123274595+DenshiJisho@users.noreply.github.com>
2023-12-28 22:24:50 -0500

    Day 8 Part 2

--- src/day8/day8.spec.ts
+++ src/day8/day8.spec.ts
@@ -1,6 +1,6 @@
 import day8 from './index';
 
-const example_input1 = `RL
+const example_input1a = `RL
 
 AAA = (BBB, CCC)
 BBB = (DDD, EEE)
@@ -10,20 +10,37 @@ EEE = (EEE, EEE)
 GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)`;
 
-const example_input2 = `LLR
+const example_input1b = `LLR
 
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)`;
 
+const example_input2 = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`;
+
 describe('On Day 8', () =>{
-    it(`part1`, ()=>{
-        expect(day8.solveForPartOne(example_input1)).toBe('2');
+    it(`part1 a`, ()=>{
+        expect(day8.solveForPartOne(example_input1a)).toBe('2');
     })
 });
 
 describe('On Day 8', () =>{
-    it(`part1 2`, ()=>{
-        expect(day8.solveForPartOne(example_input2)).toBe('6');
+    it(`part1 b`, ()=>{
+        expect(day8.solveForPartOne(example_input1b)).toBe('6');
+    })
+});
+
+describe('On Day 8', () =>{
+    it(`part2`, ()=>{
+        expect(day8.solveForPartTwo(example_input2)).toBe('6');
     })
 });
--- src/day8/index.ts
+++ src/day8/index.ts
@@ -23,10 +23,10 @@ class Day8 extends Day {
     return network;
   }
 
-  private walk (network: Map<string, Node>, directions: string): number {
+  private walk (network: Map<string, Node>, directions: string, start: string, end: (node: string) => boolean): number {
     let steps = 0;
-    let currentNode = 'AAA';
-    while (currentNode !== 'ZZZ') {
+    let currentNode = start;
+    while (!end(currentNode)) {
       if (directions[steps % directions.length] === 'L') {
         currentNode = network.get(currentNode)!.left;
       } else {
@@ -37,14 +37,28 @@ class Day8 extends Day {
     return steps;
   }
 
+  private leastCommonMultiple (nums: number[]): number {
+    const gcf = (a: number, b: number): number => b === 0 ? a : gcf(b, a % b);
+    const lcm = (a: number, b: number): number => a / gcf(a, b) * b;
+    return nums.reduce(lcm, 1);
+  }
+
   solveForPartOne (input: string): string {
     const directions = input.split('\n')[0].trim();
     const network = this.buildNetwork(input.split('\n').slice(2));
-    return String(this.walk(network, directions));
+    return String(this.walk(network, directions, 'AAA', (node) => node === 'ZZZ'));
   }
 
   solveForPartTwo (input: string): string {
-    return input;
+    const directions = input.split('\n')[0].trim();
+    const network = this.buildNetwork(input.split('\n').slice(2));
+    return String(
+      this.leastCommonMultiple(
+        [...network.entries()]
+          .filter(([key, _]) => key[2] === 'A')
+          .map(([name, _]) => this.walk(network, directions, name, (node) => node.at(-1) === 'Z'))
+      )
+    );
   }
 }
 
